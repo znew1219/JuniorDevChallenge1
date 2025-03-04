2831f4dc947ccc8ced30124b57bcae04c98acde5
znew1219 <znew1219@gmai.com>
2025-03-04 17:19:41 +0800

    修改JuniorDevChallenge1

--- Program.cs
+++ Program.cs
@@ -10,15 +10,23 @@
             string again = "Y";
 
             // 使用 while 迴圈允許使用者不斷輸入學生資料
-            while (again == "Y")
+            while (again.ToUpper() == "Y")
             {
                 Console.WriteLine("請輸入學生姓名: ");
                 string n = Console.ReadLine(); // 讀取學生姓名
+                
+                if (string.IsNullOrEmpty(n)) { break; }
+
                 names.Add(n);
 
                 Console.WriteLine("請輸入學生成績: ");
-                int g = Convert.ToInt32(Console.ReadLine());
-                grades.Add(g);
+
+                //int g = Convert.ToInt32(Console.ReadLine());
+                int g = 0; 
+                if(int.TryParse(Console.ReadLine(),out g))
+                    grades.Add(g);
+                else
+                    grades.Add(-1);
 
                 Console.WriteLine("是否要輸入另一位學生？(Y/N)");
                 again = Console.ReadLine();
@@ -27,7 +35,11 @@
             // 根據學生成績輸出對應的等級
             for (int i = 0; i < names.Count; i++)
             {
-                if (grades[i] >= 90)
+                if (grades[i] == -1)
+                {
+                    Console.WriteLine(names[i] + " 成績格式不符");
+                }
+                else if (grades[i] >= 90)
                 {
                     Console.WriteLine(names[i] + " 的成績是 A");
                 }
